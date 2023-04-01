88b6de35dc783f6b7db615a6ba1c53ea29ce295a
Tariqul <tariqularmsfitt018@gmail.com>
2023-04-01 17:54:06 +0600

    some correction on Sidecart components

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -4,10 +4,10 @@ const SideCart = props => {
   console.log(props.readTime);
   return (
     <div className="text-center mb-10 max-w-7xl mx-auto bg-slate-400 rounded-lg px-1 md:px-6 py-3">
-      <h3 className="md:text-3xl text-2xl font-bold text-white italic">
+      <h3 className="md:text-3xl text-2xl font-semibold text-white">
         Spent time on read : {props.readTime}
       </h3>
-      <h3 className="md:text-3xl text-2xl font-bold mt-3 italic">
+      <h3 className="md:text-3xl text-2xl font-semibold mt-3 ">
         Bookmarked Blogs :{props.bookmarks.length}
       </h3>
       {props.bookmarks.map(title => (
